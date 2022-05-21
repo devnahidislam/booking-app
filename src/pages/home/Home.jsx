2fa9fe9636d749c299ff5dffef6b8e3db2352fdc
mdnahidislam <mdnahidislam30@gmail.com>
2022-05-22 01:26:55 +0600

    Featured Properties and Mail List section are done.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import Navbar from '../components/navbar/Navbar';
 import Header from '../components/header/Header';
 import Featured from '../components/featured/Featured';
 import PropertyList from '../components/propertyList/PropertyList';
+import FeaturedProperties from '../components/featuredProperties/FeaturedProperties';
+import MailList from '../components/mailList/MailList';
 
 
 const Home = () => {
@@ -14,6 +16,9 @@ const Home = () => {
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+        <MailList />
       </div>
     </div>
   )
