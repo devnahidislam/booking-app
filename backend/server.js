8228663e5ceac973cc7275cd5a59454ebda049f6
mdnahidislam <mdnahidislam30@gmail.com>
2022-05-26 22:34:39 +0600

    backend Adding

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const express = require('express');
+const app = express();
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
