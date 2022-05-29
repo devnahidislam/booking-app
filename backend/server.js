d52c891747130a030f50e5e2c46bcb7fe6e933f9
mdnahidislam <mdnahidislam30@gmail.com>
2022-05-29 22:25:18 +0600

    Hotels add route created

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import authRoute from './routers/auth.js'
+import userRoute from './routers/user.js'
+import hotelsRoute from './routers/hotels.js'
+import roomsRoute from './routers/rooms.js'
 import dotenv from 'dotenv'
 dotenv.config();
 import connectDB from './config/db.js'
@@ -9,7 +12,12 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json())
+
 // Middlewares
-app.use("/auth", authRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
