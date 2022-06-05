6ed8d193beb48a03910eaef2f6af80c56b2eab81
mdnahidislam <mdnahidislam30@gmail.com>
2022-06-05 11:28:43 +0600

    Login route created

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import { createError } from './../utils/error.js';
 
 export const register = async (req, res, next) => {
   try {
@@ -17,4 +18,20 @@ export const register = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-}
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username : req.body.username });
+    if (!user) return next(createError(404, "User Not Found."));
+    
+    const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+    if (!isPasswordCorrect) return next(createError(400, "Incorrect Password."));
+
+    const { password, isAdmin, ...otherDetails } = user._doc;
+    res.status(201).json(otherDetails);
+
+  } catch (error) {
+    next(error);
+  }
+};
